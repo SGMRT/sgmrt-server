3bb79a07710d0c4094990768cbd4413567beb38f
이진 <ctce7226@gmail.com>
2025-06-16 16:25:15 +0900

    feat: 예외 상황에서 공통으로 응답할 객체 및 코드 정의

---
+++ src/main/java/soma/ghostrunner/global/common/error/ErrorCode.java
@@ -0,0 +1,29 @@
+package soma.ghostrunner.global.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    // Common
+    INVALID_REQUEST_VALUE("C-001",  BAD_REQUEST, "잘못된 요청 데이터"),
+    MISSING_REQUESTED_DATA("C-002",  BAD_REQUEST, "필수 파라미터 누락"),
+    METHOD_NOT_ALLOWED("C-003",  HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메서드"),
+    NONE_REQUEST_URI("C-004",  NOT_FOUND, "잘못된 요청 URI"),
+    SERVICE_UNAVAILABLE("C-005",  HttpStatus.SERVICE_UNAVAILABLE, "서비스 문제 발생"),
+    TOO_MANY_REQUESTS("C-006",  HttpStatus.TOO_MANY_REQUESTS, "요청 횟수 초과"),
+
+    // Member
+    MEMBER_NOT_FOUND("M-001", NOT_FOUND, "존재하지 않는 회원"),
+    MEMBER_ALREADY_EXISTED("M-002", CONFLICT, "이미 존재하는 회원"),
+    ;
+
+    private final String code;
+    private final HttpStatus status;
+    private final String message;
+}
---
+++ src/main/java/soma/ghostrunner/global/common/error/ErrorResponse.java
@@ -0,0 +1,72 @@
+package soma.ghostrunner.global.common.error;
+
+import lombok.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+
+    private String code;
+    private String message;
+    private List<FieldErrorInfo> fieldErrorInfos;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private ErrorResponse(String code, String message, List<FieldErrorInfo> fieldErrorInfos) {
+        this.code = code;
+        this.message = message;
+        this.fieldErrorInfos = fieldErrorInfos;
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, BindingResult bindingResult) {
+        List<FieldErrorInfo> fieldErrorInfos = Optional.ofNullable(bindingResult)
+                .map(FieldErrorInfo::from)
+                .orElseGet(Collections::emptyList);
+
+        return ErrorResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .fieldErrorInfos(fieldErrorInfos)
+                .build();
+    }
+
+    public static ErrorResponse from(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .fieldErrorInfos(Collections.emptyList())
+                .build();
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldErrorInfo {
+        private String field;
+        private String value;
+        private String reason;
+
+        @Builder(access = AccessLevel.PRIVATE)
+        private FieldErrorInfo(String field, String value, String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        private static List<FieldErrorInfo> from(BindingResult bindingResult) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream().
+                    map(error -> FieldErrorInfo.builder()
+                            .field(error.getField())
+                            .value((error.getRejectedValue() == null) ? null : error.getRejectedValue().toString())
+                            .reason(error.getDefaultMessage())
+                            .build())
+                    .collect(Collectors.toList());
+        }
+    }
+}
