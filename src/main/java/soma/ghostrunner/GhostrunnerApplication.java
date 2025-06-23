5eecaf380d4a5902b52f347c4de25ebd6c9c82d0
이진 <ctce7226@gmail.com>
2025-06-23 11:37:47 +0900

    feat: BaseTimeEntity 추가

--- src/main/java/soma/ghostrunner/GhostrunnerApplication.java
+++ src/main/java/soma/ghostrunner/GhostrunnerApplication.java
@@ -2,7 +2,9 @@ package soma.ghostrunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class GhostrunnerApplication {
 
