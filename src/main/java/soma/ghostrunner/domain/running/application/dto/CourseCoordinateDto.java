6acddb8e2764bbba8d2865a0d2b0ac5d631fd191
JaehwanH <jec0101@naver.com>
2025-07-05 00:28:54 +0900

    add(CourseCoordinateDto): @Builder 추가

--- src/main/java/soma/ghostrunner/domain/running/application/dto/CourseCoordinateDto.java
+++ src/main/java/soma/ghostrunner/domain/running/application/dto/CourseCoordinateDto.java
@@ -1,11 +1,13 @@
 package soma.ghostrunner.domain.running.application.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CourseCoordinateDto {
