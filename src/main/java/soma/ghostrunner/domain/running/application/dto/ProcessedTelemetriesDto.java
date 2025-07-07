8f83846ccad733bf5df837cac06cb5bd27100864
이진 <ctce7226@gmail.com>
2025-07-08 08:41:47 +0900

    refactor: Business Layer 단에서 사용하는 DTO에 대해 불필요한 @Data 롬복 제거

--- src/main/java/soma/ghostrunner/domain/running/application/dto/ProcessedTelemetriesDto.java
+++ src/main/java/soma/ghostrunner/domain/running/application/dto/ProcessedTelemetriesDto.java
@@ -2,13 +2,13 @@ package soma.ghostrunner.domain.running.application.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import soma.ghostrunner.domain.course.domain.StartPoint;
 
 import java.util.List;
 
-@Data @Builder
-@AllArgsConstructor
+@Getter
+@AllArgsConstructor @Builder
 public class ProcessedTelemetriesDto {
     private List<TelemetryDto> relativeTelemetries;
     private StartPoint startPoint;
