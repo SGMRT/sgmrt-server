e447ca7c9fbdc48aac11592bf722977d60f06c1f
bokdoong <102129660+BokDoong@users.noreply.github.com>
2025-07-11 09:13:07 +0900

    [SGMR-265] CloudWatch 로그 전송 위한 Logger, logback 구성 및 API 수정 (#22)
    * feat: Dev, Prod 환경에서 로그를 CloudWatch로 전송하기 위한 Logger 개발
    
    * feat: Dev, Prod 환경에서 로그를 CloudWatch로 전송하기 위한 Logger 개발
    
    * refactor: RunningTelemetryQueryService에서는 시계열만 조회하고 RunningQueryService에서 시계열을 DTO에 넣도록 분리
    
    * refactor: telemetries가 빈 배열로 들어올 때 5xx 에러가 발생하는 것을 4xx로 던지도록 수정

--- src/main/java/soma/ghostrunner/global/common/log/HttpLogger.java
+++ src/main/java/soma/ghostrunner/global/common/log/HttpLogger.java
@@ -1,69 +1,17 @@
 package soma.ghostrunner.global.common.log;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Component
 public class HttpLogger {
 
-    // 로깅
     public void log(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response, double elapsedTime) {
-        StringBuffer logBuffer = new StringBuffer();
-
-        // Request 메타데이터 로깅
-        logBuffer.append("\n");
-        logBuffer.append(">> " + request.getMethod() + " " + request.getRequestURI() + " " + parseResponseStatus(response) + " " + elapsedTime).append("s\n");
-        logBuffer.append(">> Client IP: " + request.getRemoteAddr()).append("\n");
-        logBuffer.append(">> Headers: ").append(parseRequestHeaders(request)).append("\n");
-
-        // 쿼리 스트링
-        String queryString = request.getQueryString();
-        logBuffer.append(">> Request Param: ").append(queryString != null ? queryString : "None").append("\n");
-
-        // Request Body
-        logBuffer.append(">> Request Body: ").append("\n").append(parseRequestBody(request)).append("\n");
-
-        // Response Body
-        logBuffer.append(">> Response Body: ").append("\n").append(parseResponseBody(response)).append("\n");
-
-        // 로그
-        log.info(logBuffer.toString());
-    }
-
-    private String parseResponseStatus(HttpServletResponse response) {
-        HttpStatus responseStatus = HttpStatus.valueOf(response.getStatus());
-        return String.valueOf(responseStatus.value());
-    }
-
-    private Map<String, Object> parseRequestHeaders(ContentCachingRequestWrapper request) {
-        Map<String, Object> headerMap = new HashMap<>();
-
-        Enumeration<String> headers = request.getHeaderNames();
-        while (headers.hasMoreElements()) {
-            String headerName = headers.nextElement();
-            headerMap.put(headerName, request.getHeader(headerName));
-        }
-        return headerMap;
-    }
-
-    private String parseRequestBody(ContentCachingRequestWrapper request) {
-        byte[] content = request.getContentAsByteArray();
-        return content.length == 0 ? "Empty Body" : new String(content, StandardCharsets.UTF_8);
-    }
-
-    private String parseResponseBody(ContentCachingResponseWrapper response) {
-        byte[] content = response.getContentAsByteArray();
-        return content.length == 0 ? "Empty Body" : new String(content, StandardCharsets.UTF_8);
+        HttpLogMessage logMessage = HttpLogMessage.of(request, response, elapsedTime);
+        log.info(logMessage.toPrettierLog());
     }
 
 }
