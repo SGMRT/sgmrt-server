8daf8c4b1d1afc945639a8d81a3c41cb5fa71257
Jaehwan Hyun <54016683+ja7811@users.noreply.github.com>
2025-07-16 15:40:19 +0900

    [SGMR-339] S3 Presigned Url 생성기 및 회원 프로필 이미지 업로드 API 추가 (#29)
    * add: MemberController 추가
    
    * move(Gender): Gender 패키지 enums로 이동
    
    * feat: S3 Presigned URL 생성기 구현
    
    * update: API 반환타입 Object -> String 변경
    
    * update(MemberService): UUID.randomUUID()에 불필요한 toString() 제거
    
    * chores: AwsConfig 마지막줄 빈줄 추가

---
+++ src/main/java/soma/ghostrunner/domain/member/api/MemberController.java
@@ -0,0 +1,23 @@
+package soma.ghostrunner.domain.member.api;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import soma.ghostrunner.domain.member.api.dto.ProfileImageUploadRequest;
+import soma.ghostrunner.domain.member.application.MemberService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/{memberUuid}/profile-image/upload-url")
+    public String generateProfileImageUploadUrl(
+            @PathVariable("memberUuid") String memberUuid,
+            @RequestBody @Valid ProfileImageUploadRequest request) {
+        return memberService.generateProfileImageUploadUrl(memberUuid, request);
+
+    }
+}
