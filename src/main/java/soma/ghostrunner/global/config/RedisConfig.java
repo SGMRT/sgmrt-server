27dea5f2a6a3fb10ce86372aa45b844b453001b9
이진 <ctce7226@gmail.com>
2025-07-21 15:42:13 +0900

    chore: REDIS의 TLS 설정 추가

--- src/main/java/soma/ghostrunner/global/config/RedisConfig.java
+++ src/main/java/soma/ghostrunner/global/config/RedisConfig.java
@@ -5,15 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
