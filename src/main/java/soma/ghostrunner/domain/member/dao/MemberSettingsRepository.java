f0a9df8ba36b3109290360b1102aac84b554f05e
JaehwanH <jec0101@naver.com>
2025-07-27 18:32:04 +0900

    add(MemberSettings): MemberSettings 및 Repository 추가

---
+++ src/main/java/soma/ghostrunner/domain/member/dao/MemberSettingsRepository.java
@@ -0,0 +1,12 @@
+package soma.ghostrunner.domain.member.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import soma.ghostrunner.domain.member.domain.MemberSettings;
+
+import java.util.Optional;
+
+public interface MemberSettingsRepository extends JpaRepository<MemberSettings, Long> {
+
+    Optional<MemberSettings> findByMember_Uuid(String memberUuid);
+
+}
---
+++ src/main/java/soma/ghostrunner/domain/member/domain/MemberSettings.java
@@ -0,0 +1,76 @@
+package soma.ghostrunner.domain.member.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberSettings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Member member;
+
+    @Column(nullable = false)
+    private boolean pushAlarmEnabled;
+
+    @Column(nullable = false)
+    private boolean vibrationEnabled;
+
+    @Column(nullable = false)
+    private boolean doNotDisturbAtNightEnabled;
+
+    public void updateSettings(Boolean pushAlarmEnabled, Boolean vibrationEnabled, Boolean doNotDisturbAtNightEnabled) {
+        this.pushAlarmEnabled = pushAlarmEnabled != null ? pushAlarmEnabled : this.pushAlarmEnabled;
+        this.vibrationEnabled = vibrationEnabled != null ? vibrationEnabled : this.vibrationEnabled;
+        this.doNotDisturbAtNightEnabled = doNotDisturbAtNightEnabled != null
+                ? doNotDisturbAtNightEnabled
+                : this.doNotDisturbAtNightEnabled;
+    }
+
+
+    @Builder(access = AccessLevel.PRIVATE)
+    protected MemberSettings(Member member, Boolean pushAlarmEnabled,
+                             Boolean vibrationEnabled, Boolean doNotDisturbAtNightEnabled) {
+        this.member = member;
+        this.pushAlarmEnabled = pushAlarmEnabled;
+        this.vibrationEnabled = vibrationEnabled;
+        this.doNotDisturbAtNightEnabled = doNotDisturbAtNightEnabled;
+    }
+
+    public static MemberSettings of(Member member) {
+        if (member == null) throw new IllegalArgumentException("Member cannot be null");
+        return MemberSettings.builder()
+                .member(member)
+                .pushAlarmEnabled(true)
+                .vibrationEnabled(true)
+                .doNotDisturbAtNightEnabled(true)
+                .build();
+    }
+
+    public static MemberSettings of(Member member, boolean pushAlarmEnabled,
+                                    boolean vibrationEnabled, boolean doNotDisturbAtNightEnabled) {
+        if (member == null) throw new IllegalArgumentException("Member cannot be null");
+        return MemberSettings.builder()
+                .member(member)
+                .pushAlarmEnabled(pushAlarmEnabled)
+                .vibrationEnabled(vibrationEnabled)
+                .doNotDisturbAtNightEnabled(doNotDisturbAtNightEnabled)
+                .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MemberSettings other)) return false;
+        return (this.member.getId().equals(other.member.getId()))
+            && (this.pushAlarmEnabled == other.pushAlarmEnabled)
+            && (this.vibrationEnabled == other.vibrationEnabled)
+            && (this.doNotDisturbAtNightEnabled == other.doNotDisturbAtNightEnabled);
+    }
+
+}
