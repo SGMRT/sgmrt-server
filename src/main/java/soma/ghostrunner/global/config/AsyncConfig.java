d835382450ef4f827aff486b68f732668b92a3de
JaehwanH <jec0101@naver.com>
2025-09-08 03:52:18 +0900

    add(configs): Expo SDK 빈 등록 & 푸쉬 알림 전송용 스레드풀 빈 등록

---
+++ src/main/java/soma/ghostrunner/global/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package soma.ghostrunner.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "pushTaskExecutor")
+    public Executor pushTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(6);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("PushThread-");
+        executor.initialize();
+        return executor;
+    }
+
+}
