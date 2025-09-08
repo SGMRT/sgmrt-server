856741a57eb2df345367b9de7e47c31ba08b615e
JaehwanH <jec0101@naver.com>
2025-09-09 00:36:57 +0900

    fix(ExpoPushClient): 비동기로 작동하지 않던 오류 해결 (supplyAsync 대신 completedFuture를 반환했었음)

--- src/main/java/soma/ghostrunner/domain/notification/client/ExpoPushClient.java
+++ src/main/java/soma/ghostrunner/domain/notification/client/ExpoPushClient.java
@@ -4,6 +4,7 @@ import com.niamedtech.expo.exposerversdk.ExpoPushNotificationClient;
 import com.niamedtech.expo.exposerversdk.request.PushNotification;
 import com.niamedtech.expo.exposerversdk.response.TicketResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import soma.ghostrunner.domain.notification.application.dto.NotificationRequest;
@@ -13,35 +14,48 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.Executor;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ExpoPushClient {
 
     private final ExpoPushNotificationClient pushClient;
+    private final Executor pushTaskExecutor;
 
-    @Async("pushTaskExecutor")
     public CompletableFuture<List<NotificationSendResult>> pushAsync(NotificationRequest request) {
-        try {
-            PushNotification notification = createPushNotification(request);
-            List<TicketResponse.Ticket> tickets = pushClient.sendPushNotifications(List.of(notification));
-
-            List<NotificationSendResult> results = new ArrayList<>();
-            int i = 0;
-            for(var ticket : tickets) {
-                switch (ticket.getStatus()) {
-                    case OK -> results.add(NotificationSendResult
-                                .ofSuccess(request.getIds().get(i).getNotificationId(), ticket.getId()));
-                    case ERROR -> results.add(NotificationSendResult
-                                .ofFailure(request.getIds().get(i).getNotificationId(), ticket.getMessage()));
+            return CompletableFuture.supplyAsync(() -> {
+                try {
+                    PushNotification notification = createPushNotification(request);
+                    List<TicketResponse.Ticket> tickets = pushClient.sendPushNotifications(List.of(notification));
+                    return mapToNotificationSendResults(request, tickets);
+                } catch (Exception e) {
+                    log.error("ExpoPushClient: Expo push failed with exception for request {}: {}", request, e.getMessage(), e);
+                    throw new CompletionException(e);
                 }
-                i++;
-            }
-            return CompletableFuture.completedFuture(results);
-        } catch (IOException e) {
-            // todo 나중에 채우기
-            return CompletableFuture.failedFuture(e);
+            }, pushTaskExecutor);
+    }
+
+    private static List<NotificationSendResult> mapToNotificationSendResults(NotificationRequest request,
+                                                                             List<TicketResponse.Ticket> tickets) {
+        List<NotificationSendResult> results = new ArrayList<>();
+        if(tickets.size() != request.getIds().size()) {
+            throw new RuntimeException("Ticket size and request size do not match - tickets: " + tickets.size() + ", request: " + request.getIds().size());
         }
+
+        int i = 0;
+        for(var ticket : tickets) {
+            switch (ticket.getStatus()) {
+                case OK -> results.add(NotificationSendResult
+                            .ofSuccess(request.getIds().get(i).getNotificationId(), ticket.getId()));
+                case ERROR -> results.add(NotificationSendResult
+                            .ofFailure(request.getIds().get(i).getNotificationId(), ticket.getMessage()));
+            }
+            i++;
+        }
+        return results;
     }
 
     private PushNotification createPushNotification(NotificationRequest request) {
