b5967b921086ccd548ba27ee7003f5d13be3823b
이진 <ctce7226@gmail.com>
2025-09-10 13:46:48 +0900

    chore: 테스트용 메서드 제거

--- src/main/java/soma/ghostrunner/global/config/WebClientConfig.java
+++ src/main/java/soma/ghostrunner/global/config/WebClientConfig.java
@@ -21,20 +21,4 @@ public class WebClientConfig {
                 .build();
     }
 
-    @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        // 1. Netty의 이벤트 루프 리소스를 8개의 스레드를 가진 고정 스레드 풀을 생성
-        LoopResources loopResources = LoopResources.create("my-netty-threads", 8, true);
-
-        // 2. 위에서 만든 LoopResources를 사용하도록 HttpClient를 설정
-        HttpClient httpClient = HttpClient.create()
-                .runOn(loopResources);
-
-        // 3. WebClient가 커스텀 HttpClient를 사용하도록 설정합니다.
-        return builder
-                .baseUrl("http://localhost:3000")
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
-    }
-
 }
