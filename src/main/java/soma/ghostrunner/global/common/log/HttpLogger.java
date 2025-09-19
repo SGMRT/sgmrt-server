0dd67582f92645df6a0625c38ef1415387682be2
JaehwanH <jec0101@naver.com>
2025-09-19 16:20:21 +0900

    update(HttpLogger): 헬스체크 API 로깅 제외

--- src/main/java/soma/ghostrunner/global/common/log/HttpLogger.java
+++ src/main/java/soma/ghostrunner/global/common/log/HttpLogger.java
@@ -5,11 +5,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
+import java.util.Set;
+
 @Slf4j
 @Component
 public class HttpLogger {
 
+    private static final Set<String> LOG_EXCLUSION_URIS = Set.of(
+            "/",
+            "/favicon.ico",
+            "/actuator/prometheus",
+            "/actuator/health"
+    );
+
     public void log(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response, double elapsedTime) {
+        if (LOG_EXCLUSION_URIS.contains(request.getRequestURI())) {
+            return;
+        }
         HttpLogMessage logMessage = HttpLogMessage.of(request, response, elapsedTime);
         log.info(logMessage.toPrettierLog());
     }
