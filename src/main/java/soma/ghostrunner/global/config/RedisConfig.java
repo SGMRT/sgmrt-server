beab1cdce82e8466a8cfe1ed3392fce263b44ef1
JaehwanH <jec0101@naver.com>
2025-10-13 04:13:41 +0900

    update(RedisConfig): LettuceConnectionFactory 주입받는 부분 삭제 - 당장 필요 없을듯

--- src/main/java/soma/ghostrunner/global/config/RedisConfig.java
+++ src/main/java/soma/ghostrunner/global/config/RedisConfig.java
@@ -1,30 +1,14 @@
 package soma.ghostrunner.global.config;
 
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    @Primary
-    public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
-        RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration();
-        standaloneConfig.setHostName(redisProperties.getHost());
-        standaloneConfig.setPort(redisProperties.getPort());
-        if (redisProperties.getPassword() != null) {
-            standaloneConfig.setPassword(redisProperties.getPassword());
-        }
-        return new LettuceConnectionFactory(standaloneConfig);
-    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
