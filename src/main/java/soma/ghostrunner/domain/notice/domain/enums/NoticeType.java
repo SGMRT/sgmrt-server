8e1fcf248fabcf0c2b9f6895d6510e1e07a64870
JaehwanH <jec0101@naver.com>
2025-10-22 14:09:45 +0900

    update(NoticeType): GENERAL_V2, EVENT_V2 타입 추가

--- src/main/java/soma/ghostrunner/domain/notice/domain/enums/NoticeType.java
+++ src/main/java/soma/ghostrunner/domain/notice/domain/enums/NoticeType.java
@@ -1,6 +1,17 @@
 package soma.ghostrunner.domain.notice.domain.enums;
 
+import java.util.List;
+
 public enum NoticeType {
+    // DEPRECATED TYPES
     GENERAL,
-    EVENT
+    EVENT,
+
+    // NEW TYPES
+    GENERAL_V2,
+    EVENT_V2;
+
+    public static List<NoticeType> getDeprecatedTypes() {
+        return List.of(GENERAL, EVENT);
+    }
 }
