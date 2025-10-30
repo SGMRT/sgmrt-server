9ebe68dae1a24510212993553f60c5b48e08e2c9
JaehwanH <jec0101@naver.com>
2025-10-30 20:00:11 +0900

    add: Sqs 의존성 추가 및 빈 등록

--- src/main/java/soma/ghostrunner/global/config/AwsConfig.java
+++ src/main/java/soma/ghostrunner/global/config/AwsConfig.java
@@ -1,5 +1,9 @@
 package soma.ghostrunner.global.config;
 
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import io.awspring.cloud.sqs.listener.acknowledgement.AcknowledgementOrdering;
+import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +12,9 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.time.Duration;
 
 @Configuration
 public class AwsConfig {
@@ -38,6 +45,35 @@ public class AwsConfig {
         return S3Presigner.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
+
+    @Bean
+    public SqsAsyncClient sqsAsyncClient() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return SqsAsyncClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
+
+    @Bean
+    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient) {
+        return SqsTemplate.newTemplate(sqsAsyncClient);
+    }
+
+    @Bean
+    public SqsMessageListenerContainerFactory<Object> sqsMessageListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
+        return SqsMessageListenerContainerFactory.builder()
+                .sqsAsyncClient(sqsAsyncClient)
+                .configure(options -> options
+                        .acknowledgementMode(AcknowledgementMode.ON_SUCCESS)
+                        .maxConcurrentMessages(10)
+                        .maxMessagesPerPoll(10)
+                        .pollTimeout(Duration.ofSeconds(5))
+                        .acknowledgementOrdering(AcknowledgementOrdering.ORDERED)
+                )
                 .build();
     }
 
