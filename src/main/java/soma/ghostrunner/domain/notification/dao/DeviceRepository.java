662316111176905bc473dfd9eb54a8aee28ade49
JaehwanH <jec0101@naver.com>
2025-11-05 00:42:26 +0900

    add(CustomDeviceRepository & Impl): 주어진 회원 ID와 버전 범위 내의 Device 목록 조회 메소드 구현

--- src/main/java/soma/ghostrunner/domain/notification/dao/DeviceRepository.java
+++ src/main/java/soma/ghostrunner/domain/notification/dao/DeviceRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface DeviceRepository extends JpaRepository<Device, Long> {
+public interface DeviceRepository extends JpaRepository<Device, Long>, CustomDeviceRepository {
 
     boolean existsByMemberIdAndToken(Long memberId, String token);
 
