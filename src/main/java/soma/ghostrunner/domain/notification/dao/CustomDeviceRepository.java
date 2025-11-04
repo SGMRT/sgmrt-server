64b99f22815844986ca7154bca01e581f8034637
JaehwanH <jec0101@naver.com>
2025-11-05 00:41:22 +0900

    add(CustomDeviceRepository & Impl): 주어진 회원 ID와 버전 범위 내의 Device 목록 조회 메소드 구현

---
+++ src/main/java/soma/ghostrunner/domain/notification/dao/CustomDeviceRepository.java
@@ -0,0 +1,10 @@
+package soma.ghostrunner.domain.notification.dao;
+
+import soma.ghostrunner.domain.notification.domain.Device;
+import soma.ghostrunner.global.common.versioning.VersionRange;
+
+import java.util.List;
+
+public interface CustomDeviceRepository {
+    List<Device> findAllByMemberIdsAndAppVersionRange(List<Long> memberIds, VersionRange appVersionRange);
+}
