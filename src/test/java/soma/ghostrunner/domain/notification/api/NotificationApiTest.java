b5bcc46c877248f748f0e64f08aefb9327d50df5
JaehwanH <jec0101@naver.com>
2025-11-09 00:50:25 +0900

    update: SecurityConfig에 /v1/push/** 추가

--- src/test/java/soma/ghostrunner/domain/notification/api/NotificationApiTest.java
+++ src/test/java/soma/ghostrunner/domain/notification/api/NotificationApiTest.java
@@ -2,10 +2,17 @@ package soma.ghostrunner.domain.notification.api;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import soma.ghostrunner.ApiTestSupport;
+import soma.ghostrunner.global.security.jwt.JwtUserDetails;
+
+import java.util.List;
 
 import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,10 +22,19 @@ class NotificationApiTest extends ApiTestSupport {
     @Test
     void markPushNotificationAsRead_success() throws Exception {
         // given
-        var pushId = "uuid";
+        var pushId = "uuid1234";
+
+        JwtUserDetails userDetails = new JwtUserDetails("userId");
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
 
         // when & then
-        mockMvc.perform(MockMvcRequestBuilders.post("/v1/push/{pushUuid}", pushId))
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/push/{messageUuid}", pushId)
+                    .with(authentication(authentication))
+                    .with(SecurityMockMvcRequestPostProcessors.csrf()))
                 .andDo(print())
                 .andExpect(status().isOk());
 
