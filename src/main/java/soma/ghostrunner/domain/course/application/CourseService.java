53220de6c0572fdf2b4655a3fa4986f414cc3c6d
이진 <ctce7226@gmail.com>
2026-01-16 10:54:50 +0900

    feat: 코스따라 러닝 시 CourseSubscription 테이블 추가하는 로직 설계

--- src/main/java/soma/ghostrunner/domain/course/application/CourseService.java
+++ src/main/java/soma/ghostrunner/domain/course/application/CourseService.java
@@ -68,14 +68,14 @@ public class CourseService {
 
     @Transactional
     public void deleteCourse(Long courseId, String memberUuid) {
-        Course course = findCourseById(courseId);
+        Course course = findCourseByIdFetchJoinMember(courseId);
         course.verifyOwner(memberUuid);
         courseRepository.delete(course);
     }
 
     @Transactional
     public void updateCourse(Long courseId, CoursePatchRequest request, String memberUuid) {
-        Course course = findCourseById(courseId);
+        Course course = findCourseByIdFetchJoinMember(courseId);
         course.verifyOwner(memberUuid);
 
         if (request.getName() != null) {
--- src/test/java/soma/ghostrunner/domain/course/application/CourseCacheEventListenerTest.java
+++ src/test/java/soma/ghostrunner/domain/course/application/CourseCacheEventListenerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import soma.ghostrunner.domain.course.dao.CourseCacheRepository;
 import soma.ghostrunner.domain.running.domain.events.RunFinishedEvent;
 import soma.ghostrunner.domain.running.domain.events.RunUpdatedEvent;
